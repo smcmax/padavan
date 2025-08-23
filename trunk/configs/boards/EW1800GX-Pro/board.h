e5ac0fe82b01bafcb2116334d20c1c8b65017d43
vb1980 <vb1980@sina.com>
2025-08-23 09:05:28 +0800

    try to add Ruijie EW1800GX Pro support

---
+++ trunk/configs/boards/EW1800GX-Pro/board.h
@@ -0,0 +1,21 @@
+/* WIA3300-10 */
+
+#define BOARD_PID		"EW1800GX-Pro"
+#define BOARD_NAME		"EW1800GX-Pro"
+#define BOARD_DESC		"Ruijie EW1800GX Pro Wireless Router"
+#define BOARD_VENDOR_NAME	"Ruijie"
+#define BOARD_VENDOR_URL	"https://www.ruijie.com.cn/"
+#define BOARD_MODEL_URL		"https://www.ruijie.com.cn/"
+#define BOARD_BOOT_TIME		30
+#define BOARD_FLASH_TIME	120
+#define BOARD_HAS_5G_11AC		1
+#define BOARD_HAS_5G_11AX		1
+#define BOARD_HAS_2G_11AX		1
+#define BOARD_NUM_ANT_5G_TX		2
+#define BOARD_NUM_ANT_5G_RX		2
+#define BOARD_NUM_ANT_2G_TX		2
+#define BOARD_NUM_ANT_2G_RX		2
+#define BOARD_NUM_ETH_LEDS		0
+#define BOARD_NUM_ETH_EPHY		5
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
